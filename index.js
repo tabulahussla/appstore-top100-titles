623fc4df22b4d144c9d2cf46e79ab0ada2503d9e
Georgy Chirkov <mail@api.haus>
2019-09-18 20:27:15 +0300

    finish

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ node ${argv.$0} -o path to txt or csv file to save -s searchTerm -g grossingGenr
 -c country
 -l limit`)
 
-if (typeof argv.o !== 'string' || !argv.o.length) {
+const { o: outputPath = '' } = argv;
+
+if (typeof outputPath !== 'string' || !outputPath.length) {
     return PrintUsage();
 }
 
@@ -33,38 +35,65 @@ if (typeof argv.o !== 'string' || !argv.o.length) {
 (async function () {
     // continue to check arguments... but found no answers...
 
+    const {
+        c: country = 'NL',
+        s: search = '',
+        g: genreId = '',
+        l: limit = 100,
+    } = argv;
+    let [category, collection] = genreId.split(':');
+
     let applications
     // CHECK SEARCH
     if (s in argv) {
         /* IT IS SEARCH ! */
         const { results } = await SearchItunesSoftware(
         /* SEARCH TERM */ argv.s,
-        /* COUNTRY CODE */ argv.c,
+        /* COUNTRY CODE */ country,
             /********************************************/
             /*      SET [PRICE VALUE FILTERING] TO "1"
                     FOR [-p]AID APPLICATIONS
                     AND "0" FOR [-f]REE APPLICATIONS */
             argv.p === true || argv.f === true ? 1 : undefined,
-        /* LIMIT VALUE */ argv.l || 100
+        /* LIMIT VALUE */ limit
         );
         /* SAVE ARRAY OF APP TITLES */
         applications = results.map(app => app.trackName)
     }
     // IF NO SEARCH, ALSO CHECK THIS OUT
     else if (g in argv) {
+        /* RESOLVE CATEGORY */
+        category = store.category[category] || category;
+        collection = store.collection[collection] || collection;
         /* LOOKUP BY GENRE ID */
-        const { g: genreId } = argv;
-        const [category, collection] = genreId.slice('|');
-        const applications = await ListByGenreId({
+        const results = await ListByGenreId({
             category,
             collection,
             country,
             limit,
-        })
+        });
+        /* SAVE ARRAY OF APP TITLES */
+        applications = results.map(app => app.title);
+    } else {
+        return PrintUsage();
+    }
+
+    const output = fs.createWriteStream(outputPath);
+
+    if (outputPath.endsWith('.txt')) {
+        for (const title of applications) {
+            output.write(`${title}\n`);
+        }
+    } else if (outputPath.endsWith('.csv')) {
+        output.write(`POSITION,TITLE\n`);
+        for (let i = 1; i <= applications.length; i++) {
+            const title = applications[i - 1];
+            output.write(`${i},${title}\n`);
+        }
     } else {
         return PrintUsage()
     }
 
-    const output = fs.createWriteStream(argv.o)
+    output.end()
 
 }())
--- list.js
+++ list.js
@@ -6,8 +6,6 @@ async function ListByGenreId({ category, collection, country = 'NL', limit = 100
         category,
         collection,
         num: limit,
-        price,
-        term,
     };
 
     // Find top N apps in category and collection
